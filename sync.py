9275076c6f657bbf168c315e3805241e6f9421e4
patr0n <105314623+patr0nq@users.noreply.github.com>
2025-04-20 17:24:54 +0300

    Update sync.py

--- sync.py
+++ sync.py
@@ -6,7 +6,24 @@ target_path = "dragon.m3u"
 response = requests.get(source_url)
 
 if response.status_code == 200:
+    lines = response.text.splitlines()
+
+    
+    unwanted_lines = [
+        "LİSTE @DRAG10 TARAFINDAN YAPILMIŞTIR",
+        "ORTAK @SYNDRAXİC",
+        source_url,
+        "#EXTM3U"
+    ]
+
+   
+    filtered_lines = [line for line in lines if line.strip() not in unwanted_lines]
+
+    if filtered_lines and not filtered_lines[0].startswith("#EXTM3U"):
+        filtered_lines.insert(0, "#EXTM3U")
+
     with open(target_path, "w", encoding="utf-8") as f:
-        f.write(response.text)
+        f.write("\n".join(filtered_lines))
+
 else:
-    print(f"Failed to fetch source M3U. Status code: {response.status_code}")
+    print(f"Kaynak M3U alınamadı. Status code: {response.status_code}")
